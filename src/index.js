d138a07ddae402567c93ecb437c2ed6254d06697
Linielt <danielhlee912@gmail.com>
2024-09-15 14:16:35 +0100

    Wrote functions to fetch and select specific weather data

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-console.log("Hello!");
+const fetchWeatherData = async (location) => {
+  try {
+    const API_KEY = "46DYZ9Z79VTXUY8RQUZTLPAXN";
+    const response = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${API_KEY}`
+    );
+    const weatherData = await response.json();
+
+    console.log(weatherData);
+    console.log(getRequiredData(weatherData));
+  } catch (error) {
+    alert(`Location not found: ${location}`);
+  }
+};
+
+const convertFahrenheitToCelcius = (fahrenheitTemp) => {
+  return (fahrenheitTemp - 32) / (9 / 5);
+};
+
+const getRequiredData = (weatherData) => {
+  return {
+    address: weatherData.address,
+    description: weatherData.description,
+    temperature: weatherData.currentConditions.temp,
+    icon: weatherData.currentConditions.icon,
+  };
+};
+
+fetchWeatherData("london");
